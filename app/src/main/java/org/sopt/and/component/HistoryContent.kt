eb7a7903891ef2b14f08c4bc907091ebb1b9cba4
hyeumm <20221159@sungshin.ac.kr>
2024-10-10 01:59:21 +0900

    Feature #1: HistoryContent

---
+++ app/src/main/java/org/sopt/and/component/HistoryContent.kt
@@ -0,0 +1,64 @@
+package org.sopt.and.component
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Info
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.ui.theme.LightGray
+import org.sopt.and.ui.theme.White
+
+@Composable
+fun HistoryContent(
+    title: String,
+    description: String,
+    modifier: Modifier = Modifier,
+) { //네이밍 애매...
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = title,
+            style = TextStyle(
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
+                color = White
+            ),
+            modifier = Modifier.padding(horizontal = 5.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(40.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.Info,
+                contentDescription = null,
+                tint = LightGray,
+                modifier = Modifier.size(80.dp)
+            )
+            Text(
+                text = description,
+                style = TextStyle(
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = LightGray
+                ),
+                modifier = Modifier.padding(top = 20.dp)
+            )
+        }
+    }
+}
