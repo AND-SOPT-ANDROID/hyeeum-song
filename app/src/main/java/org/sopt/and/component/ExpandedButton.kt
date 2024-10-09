23053f37361ca8aed03a1701f9899e30a3b7b054
hyeumm <20221159@sungshin.ac.kr>
2024-10-09 23:43:05 +0900

    Feature #1: ExpandedButton

---
+++ app/src/main/java/org/sopt/and/component/ExpandedButton.kt
@@ -0,0 +1,42 @@
+package org.sopt.and.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.sopt.and.ui.theme.Blue
+import org.sopt.and.ui.theme.Gray
+import org.sopt.and.ui.theme.White
+
+@Composable
+fun ExpandedButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = false
+) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .fillMaxWidth()
+            .clickable { onClick() }
+            .background(
+                color = if (enabled) Gray else Blue,
+            )
+    ) {
+        Text(
+            modifier = Modifier.padding(vertical = 15.dp),
+            text = text,
+            style = TextStyle(fontSize = 20.sp),
+            color = White
+        )
+    }
+}
