4f27b5d365737b49608dcec4eb08c4aad66c40ec
hyeumm <20221159@sungshin.ac.kr>
2024-10-11 06:37:05 +0900

    Feature #1: MyActivity

--- app/src/main/java/org/sopt/and/my/MyActivity.kt
+++ app/src/main/java/org/sopt/and/my/MyActivity.kt
@@ -1,4 +1,26 @@
 package org.sopt.and.my
 
-class MyActivity {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import org.sopt.and.sharedpreference.User
+import org.sopt.and.ui.theme.ANDANDROIDTheme
+
+class MyActivity : ComponentActivity() {
+    private lateinit var user: User
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        user = User(this)
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ANDANDROIDTheme {
+                MyRoute(
+                    email = user.getEmail().toString(),
+                )
+            }
+        }
+    }
 }
