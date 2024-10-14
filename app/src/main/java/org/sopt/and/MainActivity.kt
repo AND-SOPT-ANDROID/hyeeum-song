f98d2451267a262ce0c11a74043e52c124ff2a46
hyeumm <20221159@sungshin.ac.kr>
2024-10-14 18:29:05 +0900

    Delete #1: delete setContent

--- app/src/main/java/org/sopt/and/MainActivity.kt
+++ app/src/main/java/org/sopt/and/MainActivity.kt
@@ -3,13 +3,11 @@ package org.sopt.and
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import dagger.hilt.android.AndroidEntryPoint
 import org.sopt.and.my.MyActivity
 import org.sopt.and.sharedpreference.User
 import org.sopt.and.signin.SignInActivity
-import org.sopt.and.ui.theme.ANDANDROIDTheme
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -20,21 +18,17 @@ class MainActivity : ComponentActivity() {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            ANDANDROIDTheme {
-                if (user.getSignInState()) {
-                    Intent(this, MyActivity::class.java).apply {
-                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(this)
-                        finish()
-                    }
-                } else {
-                    Intent(this, SignInActivity::class.java).apply {
-                        flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(this)
-                        finish()
-                    }
-                }
+        if (user.getSignInState()) {
+            Intent(this, MyActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(this)
+                finish()
+            }
+        } else {
+            Intent(this, SignInActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(this)
+                finish()
             }
         }
     }
