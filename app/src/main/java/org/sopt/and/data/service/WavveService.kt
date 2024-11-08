6db33001c1979317f7ec28c617ed9e360b58c5cb
hyeumm <20221159@sungshin.ac.kr>
2024-11-08 19:44:52 +0900

    Feature #11: WavveService

---
+++ app/src/main/java/org/sopt/and/data/service/WavveService.kt
@@ -0,0 +1,26 @@
+package org.sopt.and.data.service
+
+import org.sopt.and.data.dto.request.RequestSignInDto
+import org.sopt.and.data.dto.request.RequestSignUpDto
+import org.sopt.and.data.dto.response.BaseResponse
+import org.sopt.and.data.dto.response.ResponseHobbyDto
+import org.sopt.and.data.dto.response.ResponseSignInDto
+import org.sopt.and.data.dto.response.ResponseSignUpDto
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface WavveService {
+    @POST("/user")
+    suspend fun signUp(
+        @Body body: RequestSignUpDto
+    ): BaseResponse<ResponseSignUpDto>
+
+    @POST("/login")
+    suspend fun signIn(
+        @Body body: RequestSignInDto
+    ): BaseResponse<ResponseSignInDto>
+
+    @GET("/user/my-hobby")
+    suspend fun getHobby(): BaseResponse<ResponseHobbyDto>
+}
