9f0cd0e08df015ff80b6467fbbfbfa6f4a3b5817
hyeumm <20221159@sungshin.ac.kr>
2024-12-06 22:47:18 +0900

    feature #13: AuthDataSource

--- app/src/main/java/org/sopt/and/data/datasource/AuthDataSource.kt
+++ app/src/main/java/org/sopt/and/data/datasource/AuthDataSource.kt
@@ -3,12 +3,10 @@ package org.sopt.and.data.datasource
 import org.sopt.and.data.dto.request.RequestSignInDto
 import org.sopt.and.data.dto.request.RequestSignUpDto
 import org.sopt.and.data.dto.response.BaseResponse
-import org.sopt.and.data.dto.response.ResponseUserHobbyDto
 import org.sopt.and.data.dto.response.ResponseSignInDto
 import org.sopt.and.data.dto.response.ResponseSignUpDto
 
-interface WavveDataSource {
+interface AuthDataSource {
     suspend fun postSignUp(requestSignUpDto: RequestSignUpDto): BaseResponse<ResponseSignUpDto>
     suspend fun postSignIn(requestSignInDto: RequestSignInDto): BaseResponse<ResponseSignInDto>
-    suspend fun getUserHobby(): BaseResponse<ResponseUserHobbyDto>
 }
