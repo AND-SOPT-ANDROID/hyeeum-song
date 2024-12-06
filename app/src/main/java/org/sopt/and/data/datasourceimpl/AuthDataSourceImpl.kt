bcba10b7a070d46c7aa3e343d9216133284f5894
hyeumm <20221159@sungshin.ac.kr>
2024-12-06 22:47:11 +0900

    feature #13: AuthDataSourceImpl

--- app/src/main/java/org/sopt/and/data/datasourceimpl/AuthDataSourceImpl.kt
+++ app/src/main/java/org/sopt/and/data/datasourceimpl/AuthDataSourceImpl.kt
@@ -1,25 +1,20 @@
 package org.sopt.and.data.datasourceimpl
 
-import org.sopt.and.data.datasource.WavveDataSource
+import org.sopt.and.data.datasource.AuthDataSource
 import org.sopt.and.data.dto.request.RequestSignInDto
 import org.sopt.and.data.dto.request.RequestSignUpDto
 import org.sopt.and.data.dto.response.BaseResponse
-import org.sopt.and.data.dto.response.ResponseUserHobbyDto
 import org.sopt.and.data.dto.response.ResponseSignInDto
 import org.sopt.and.data.dto.response.ResponseSignUpDto
-import org.sopt.and.data.service.WavveService
+import org.sopt.and.data.service.AuthService
 import javax.inject.Inject
 
-class WavveDataSourceImpl @Inject constructor(
-    private val wavveService: WavveService
-) : WavveDataSource {
+class AuthDataSourceImpl @Inject constructor(
+    private val authService: AuthService
+) : AuthDataSource {
     override suspend fun postSignUp(requestSignUpDto: RequestSignUpDto): BaseResponse<ResponseSignUpDto> =
-        wavveService.postSignUp(requestSignUpDto)
+        authService.postSignUp(requestSignUpDto)
 
     override suspend fun postSignIn(requestSignInDto: RequestSignInDto): BaseResponse<ResponseSignInDto> =
-        wavveService.postSignIn(requestSignInDto)
-
-    override suspend fun getUserHobby(): BaseResponse<ResponseUserHobbyDto> =
-        wavveService.getUserHobby()
-
+        authService.postSignIn(requestSignInDto)
 }
