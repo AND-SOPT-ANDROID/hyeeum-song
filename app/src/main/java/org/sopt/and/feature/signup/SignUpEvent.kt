63c27e97377ced4167f440e912fe9f1f871e93b8
hyeumm <20221159@sungshin.ac.kr>
2024-12-17 23:49:21 +0900

    Delete #15: delete origin SignUpEvent,SideEffect,State

--- app/src/main/java/org/sopt/and/feature/signup/SignUpEvent.kt
+++
@@ -1,7 +0,0 @@
-package org.sopt.and.feature.signup
-
-sealed class SignUpEvent {
-    data class SetUsername(val username: String) : SignUpEvent()
-    data class SetPassword(val password: String) : SignUpEvent()
-    data class SetHobby(val hobby: String) : SignUpEvent()
-}
